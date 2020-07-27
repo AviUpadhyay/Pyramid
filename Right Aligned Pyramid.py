eaace700cbb93d2e7c97c1ab17996a505d80d0e0
AviUpadhyay <avi.s.upadhyay@gmail.com>
2020-07-27 19:11:56 +0530

    Create Right Aligned Pyramid.py

---
+++ Right Aligned Pyramid.py
@@ -0,0 +1,11 @@
+#Input height
+print("Input the height you would like: ", end = " ")
+h = int(input())
+
+
+for f in range(h):
+#Nested for loop to print out '#' 
+  for i in range(f+1):
+    print("#", end = " ")
+  print(" ")
+    
